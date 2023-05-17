233edb2a1f398e5d648428e59b1a92568ce0aa3d
Inti Lauberer <fedegarcia@itba.edu.ar>
2023-05-17 18:56:22 -0300

    Base project

---
+++ Kernel/include/videoDriver.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+void putPixel(uint8_t r, uint8_t g, uint8_t b, uint32_t x, uint32_t y);
+void paintScreen(uint8_t r, uint8_t g, uint8_t b);
---
+++ Kernel/irqDispatcher.c
@@ -0,0 +1,39 @@
+#include <time.h>
+#include <stdint.h>
+#include "syscall.h"
+static void int_20();
+static void int_21();
+static uint64_t int_80(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+
+void irqDispatcher(uint64_t irq, uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
+
+	void (*interruption[256])(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8);
+	interruption[0]=&int_20;
+	interruption[1]=&int_21;
+	interruption[80]=&int_80;
+	interruption[irq](rdi,rsi,rdx,rcx,r8);
+	return;
+}
+
+void int_20() {
+	timer_handler();
+}
+
+void int_21() {
+	keyboard_handler();
+}
+
+uint64_t int_80(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8) {
+
+    switch (rdi)
+	{
+	case 1:
+		sys_write((char*)rsi, rdx, rcx);
+		break;
+	
+	case 2:
+
+		break;
+	}
+	
+}
