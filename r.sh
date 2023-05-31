bf8587e03ee81564dfa0fd3631a8b4d8933cbe1f
felipemindlin <fmindlin@itba.edu.ar>
2023-05-31 17:00:47 -0300

    implementacion de scroll parcial falta agregar buffer de screens

---
+++ r.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set audio device variable
+if [ "$(uname)" == "Darwin" ]; then
+    audio="coreaudio"
+else
+    audio="pa"
+fi
+
+qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev $audio,id=audio0 -soundhw pcspk
