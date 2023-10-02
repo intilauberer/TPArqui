67c9d1039b6561564abb0d6745156e5ac36c9b42
Federico <fedegarcia@itba.edu.ar>
2023-10-02 20:40:16 -0300

    final version

--- Kernel/idtLoader.c
+++ Kernel/idtLoader.c
@@ -46,5 +46,4 @@ static void setup_IDT_entry (int index, uint64_t offset) {
   idt[index].access = ACS_INT;
   idt[index].cero = 0;
   idt[index].other_cero = (uint64_t) 0;
-  idt[index].access = ACS_CODE;
 }
